c44341396460304c109a7a35282e6b70cb49bea6
bahrulrozak <bahrulrozak02@gmail.com>
2025-02-06 20:24:41 +0700

    Membuat sidebar untuk superadmin

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,50 @@
+@extends('admin.master')
+
+@section('content')
+<div class="container-fluid">
+    <!-- Small boxes (Stat box) -->
+    <div class="row">
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-info">
+                <div class="inner">
+                    <h3>150</h3>
+
+                    <p>Total Registration</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-success">
+                <div class="inner">
+                    <h3>150</h3>
+
+                    <p>Total All Patients</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-warning">
+                <div class="inner">
+                    <h3>150</h3>
+
+                    <p>Total All Employees</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-6">
+            <!-- small box -->
+            <div class="small-box bg-danger">
+                <div class="inner">
+                    <h3>150</h3>
+
+                    <p>Total Reports</p>
+                </div>
+            </div>
+        </div>
+        <!-- ./col -->
+    </div>
+</div>
+@endsection
